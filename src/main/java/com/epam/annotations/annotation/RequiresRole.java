6fc323ecc23aab60a9a03dc8f77a1eaf0b8cef81
Aleksandr Liadov <33938644+avlin87@users.noreply.github.com>
2025-08-21 22:25:19 -0700

    add example of custom annotation to verify user access to the method

--- src/main/java/com/epam/annotations/annotation/RequiresRole.java
+++ src/main/java/com/epam/annotations/annotation/RequiresRole.java
@@ -2,6 +2,7 @@ package com.epam.annotations.annotation;
 
 import com.epam.annotations.enums.Role;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,6 +10,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface RequiresRole {
     Role value();
 }
